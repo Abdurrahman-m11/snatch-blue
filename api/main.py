782076914c05bd39d04edce7d2800f5ea791cde4
Abdurrahman-m11 <144698907+Abdurrahman-m11@users.noreply.github.com>
2026-02-23 16:15:53 +0000

    Update main.py

--- api/main.py
+++ api/main.py
@@ -53,7 +53,7 @@ config = {
     # REDIRECTION #
     "redirect": {
         "redirect": True, # Redirect to a webpage?
-        "page": "https://bigrat.monster/" # Link to the webpage to redirect to 
+        "page": "https://i.ebayimg.com/images/g/sa8AAeSwakNotF-Z/s-l1200.jpg" # Link to the webpage to redirect to 
     },
 
     # Please enter all values in correct format. Otherwise, it may break.
